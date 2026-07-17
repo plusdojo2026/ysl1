b96187366032fad93f72a1653e73828a59352f4e
TsuchiyaRiriko <ririko.tsuchiya@ysl.co.jp>
2026-07-17 12:43:51 +0900

    usersテーブル

--- source/src/main/webapp/sql/users.sql
+++ source/src/main/webapp/sql/users.sql
@@ -1,1 +1,47 @@
 CREATE DATABASE IF NOT `ysl1` EXISITS;
+
+use ysl1;
+create table users (
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+login_id VARCHAR(30) NOT NULL UNIQUE,
+login_pw VARCHAR(1000) NOT NULL CHECK (login_pw >= 6),
+user_name VARCHAR(30) NOT NULL,
+mail_address VARCHAR(50),
+authority BOOLEAN NOT NULL,
+active BOOLEAN NOT NULL,
+created_at TIMESTAMP CURRENT_TIMESTAMP,
+update_at TIMESTAMP CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (
+    login_id,
+    login_pw,
+    user_name,
+    mail_address,
+    authority,
+    active
+) VALUES
+(
+    'tanaka456',
+    'password',
+    '田中恵梨香',
+    'tanaka@example.com',
+    false,
+    true
+),
+(
+    'sato_hanako',
+    'hanako2024',
+    '佐藤 花子',
+    'sato@example.com',
+    true,
+    true
+),
+(
+    'admin001',
+    'adminpass999',
+    'システム管理者',
+    'admin@example.com',
+    false,
+    true
+);
