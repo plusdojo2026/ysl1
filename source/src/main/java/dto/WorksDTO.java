690f8fcc2bb0d9fcdc9210d14b5f779e1814d3cf
TsuchiyaRiriko <ririko.tsuchiya@ysl.co.jp>
2026-07-17 16:57:40 +0900

    工数DTO完成

--- source/src/main/java/dto/WorksDTO.java
+++ source/src/main/java/dto/WorksDTO.java
@@ -1,5 +1,88 @@
 package dto;
 
-public class WorksDTO {
+import java.io.Serializable;
+
+public class WorksDTO implements Serializable{
+	//フィールド
+	private int id;
+	private int userId;
+	private int taskId;
+	private String workDate;
+	private String actualHours;
+	private String workDescription;
+	
+	
+	//コンストラクタ
+	public WorksDTO(int id, int userId, int taskId, String workDate, String actualHours, String workDescription) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.taskId = taskId;
+		this.workDate = workDate;
+		this.actualHours = actualHours;
+		this.workDescription = workDescription;
+	}
+
+	//引数のないコンストラクタ
+	public WorksDTO() {
+		super();
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	
+	
+	//ゲッターセッター
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getTaskId() {
+		return taskId;
+	}
+
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+
+	public String getWorkDate() {
+		return workDate;
+	}
+
+	public void setWorkDate(String workDate) {
+		this.workDate = workDate;
+	}
+
+	public String getActualHours() {
+		return actualHours;
+	}
+
+	public void setActualHours(String actualHours) {
+		this.actualHours = actualHours;
+	}
+
+	public String getWorkDescription() {
+		return workDescription;
+	}
+
+	public void setWorkDescription(String workDescription) {
+		this.workDescription = workDescription;
+	}
+	
+	
+	
+	
+	
 
 }
