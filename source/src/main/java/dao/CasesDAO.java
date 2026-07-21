9697e6c4db0947b5016e9b180c7702be945a7516
ysl-horikoshi <rihana.horikoshi@ysl.co.jp>
2026-07-21 18:30:02 +0900

    案件DAO

--- source/src/main/java/dao/CasesDAO.java
+++ source/src/main/java/dao/CasesDAO.java
@@ -17,39 +17,91 @@ public class CasesDAO {
 		this.conn = conn;
 	}
 
-	//ページを開いた時に表示するもののメソッド
-	public ArrayList<AllDTO> selectCases() throws SQLException {
+	//案件詳細を持ってくるメソッド
+	public ArrayList<AllDTO> selectCases(int id) throws SQLException {
 		ArrayList<AllDTO> allDTOs = new ArrayList<AllDTO>();
-		String sql = "select * from cases";
+		//sql文
+		String sql = "SELECT * FROM cases WHERE case_id=?";
+
 		// まとめる
 		PreparedStatement pStmt = conn.prepareStatement(sql);
-		// SELECT文を実行し、結果表を取得する
+
+		//?のための文
+		pStmt.setInt(1, id);
+
 		ResultSet rs = pStmt.executeQuery();
 
 		while (rs.next()) {
 			AllDTO dto = new AllDTO();
 			dto.setCaseId(rs.getInt("id"));
+			dto.setCaseName(rs.getString("case_name"));
+			dto.setCaseCode(rs.getInt("case_sode"));
+			dto.setCustomerName(rs.getString("customer_name"));
+			dto.setCasePriority(rs.getString("case_priority"));
+			dto.setPmId(rs.getInt("pm_id"));
+			dto.setCaseStatus(rs.getString("case_status"));
+			dto.setStartDate(rs.getString("start_date"));
+			dto.setPlannedEndDate(rs.getString("planned_end_date"));
+			dto.setCaseDescription(rs.getString("case_description"));
+			dto.setCasePlannedHours(rs.getInt("case_planned_hours"));
 
 			allDTOs.add(dto);
 		}
 		return allDTOs;
 	}
 
-	//ページを開いた時に表示するもののメソッド
-	public ArrayList<AllDTO> selectCases() throws SQLException {
+	//タスクを持ってくるメソッド
+	public ArrayList<AllDTO> selectTasks(int id) throws SQLException {
 		ArrayList<AllDTO> allDTOs = new ArrayList<AllDTO>();
-		String sql = "select * from cases WHERE";
+
+		String sql = "SELECT * FROM tasks JOIN WHERE case_id=?";
 		// まとめる
 		PreparedStatement pStmt = conn.prepareStatement(sql);
+
+		//?のための文
+		pStmt.setInt(1, id);
+
 		// SELECT文を実行し、結果表を取得する
 		ResultSet rs = pStmt.executeQuery();
 
+		//タスク名、担当者、ステータス、優先度、期間、予定/実績工数、進捗を持ってきたい
 		while (rs.next()) {
 			AllDTO dto = new AllDTO();
-			dto.setCaseId(rs.getInt("id"));
+			dto.setTaskId(rs.getInt("id"));
+			dto.setCaseId(rs.getInt("case_id"));
+			dto.setTaskName(rs.getString("task_name"));
+			dto.setCaseId(rs.getInt("manager_id"));//ここ分からん
+			dto.setTaskStatus(rs.getString("task_status"));
+			dto.setTaskPriority(rs.getString("task_priority"));
+			dto.setDeadline(rs.getString("deadline"));
+
+			dto.setTaskPlannedHours(rs.getBigDecimal("task_planned_hours"));
+			dto.setProgressRate(rs.getInt("progress_rate"));
 
 			allDTOs.add(dto);
 		}
 		return allDTOs;
 	}
+
+	//工数を持ってくるメソッド
+	public ArrayList<AllDTO> selectWorks(int id) throws SQLException {
+		ArrayList<AllDTO> allDTOs = new ArrayList<AllDTO>();
+		//sql文
+		String sql = "SELECT * FROM taskss WHERE case_id=?";
+
+		// まとめる
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+
+		//?のための文
+		pStmt.setInt(1, id);
+
+		ResultSet rs = pStmt.executeQuery();
+
+		while (rs.next()) {
+			AllDTO dto = new AllDTO();
+
+		}
+		return allDTOs;
+	}
+
 }
